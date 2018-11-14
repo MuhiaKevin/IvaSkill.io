5cc2ed11f68e419e660836506242b989c8514c64
MuhiaKevin <archerkreiger@gmail.com>
2018-11-14 18:07:40 +0300

    added signup button functionality

--- app/src/main/java/com/myfirstapp/muhia/ivaskillio/MainActivity.java
+++ app/src/main/java/com/myfirstapp/muhia/ivaskillio/MainActivity.java
@@ -1,56 +1,37 @@
 package com.myfirstapp.muhia.ivaskillio;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-     Button loginBtn;
+     Button signup;
+     private EditText username, passwd;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        signup = findViewById(R.id.signupbutton);
+        username = findViewById(R.id.textUsername);
+        passwd = findViewById(R.id.textPass);
 
-        loginBtn = findViewById(R.id.buttonLogin);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
 
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                // create a toast
-//
-//                Toast toast=Toast.makeText(getApplicationContext(),"Button Pressed by Osundwa",Toast.LENGTH_SHORT);
-//                toast.show();
-
-
-
-               // create a new activity
-//
-//                Intent i = new Intent(getApplicationContext(),Signup.class);
-//                startActivity(i);
-
-                Uri webpage;
-                webpage = Uri.parse("https://github.com/MuhiaKevin");
-
-                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-
+                Intent i = new Intent(getApplicationContext(),Signup.class);
+                startActivity(i);
 
             }
-
-
         });
-
-
     }
 
 

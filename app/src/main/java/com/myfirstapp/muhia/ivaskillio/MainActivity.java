4e8d1580160962e8ca53b7200a87d6f245e45a08
MuhiaKevin <archerkreiger@gmail.com>
2018-11-14 15:09:51 +0300

    made some changes on the design and everything are lining up

--- app/src/main/java/com/myfirstapp/muhia/ivaskillio/MainActivity.java
+++ app/src/main/java/com/myfirstapp/muhia/ivaskillio/MainActivity.java
@@ -1,21 +1,58 @@
 package com.myfirstapp.muhia.ivaskillio;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+     Button loginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginBtn = findViewById(R.id.buttonLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                // create a toast
+//
+//                Toast toast=Toast.makeText(getApplicationContext(),"Button Pressed by Osundwa",Toast.LENGTH_SHORT);
+//                toast.show();
+
+
+
+               // create a new activity
+//
+//                Intent i = new Intent(getApplicationContext(),Signup.class);
+//                startActivity(i);
+
+                Uri webpage;
+                webpage = Uri.parse("https://github.com/MuhiaKevin");
+
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+
+
+            }
+
+
+        });
+
+
     }
 
+
 }
+
